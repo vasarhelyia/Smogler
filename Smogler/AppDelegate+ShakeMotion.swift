5735c968d95473e61bdd05bb326a7b7700026a02
Dominik Bucher <dominik.bucher@showmax.com>
2018-06-04 00:04:15 +0200

    Remove redundant spacing

--- Smogler/AppDelegate+ShakeMotion.swift
+++ Smogler/AppDelegate+ShakeMotion.swift
@@ -11,7 +11,7 @@ import UIKit
 
 extension UIWindow {
     open override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        if  motion == .motionShake {
+        if motion == .motionShake {
             let alertController = UIAlertController(title: "Enter your API Token", message: nil, preferredStyle: .alert)
             alertController.addTextField { textfield in
                 textfield.placeholder = "Your generated API Token"
